d67d7f51891ae2e8528278c6b28db27df6a4e7c4
jadinsa9-sudo <jadinsa9@gmail.com>
2026-08-31 13:23:24 +0200

    Changed time updating to use Git commits

--- src/script.js
+++ src/script.js
@@ -5,20 +5,28 @@ button.addEventListener("click", function () {
     document.body.classList.toggle("dark");
 });
 
-function updateDate() {
-    const currentDate = new Date().toISOString;
+// function updateDate() {
+//     const currentDate = new Date();
 
-    localStorage.setItem("lastUpdated", currentDate);
-    displayDate(currentDate);
-}
+//     localStorage.setItem("lastUpdated", currentDate.getTime());
+//     displayDate(currentDate);
+// }
 
 function displayDate(date) {
     dateElement.textContent =
-        "Last updated: " + new Date(date).toLocaleString("en-GB");
+        "Last updated: " + date.toLocaleString("en-GB");
 }
 
+// https://api.github.com/repos/jadinsa9-sudo/portfolio/commits
+// const saveDate = localStorage.getItem("lastUpdated");
+// if (saveDate) {
+//     displayDate(new Date(parseInt(saveDate)));
+// }
 
-const saveDate = localStorage.getItem("lastUpdated");
-if (saveDate) {
-    displayDate(savedDate);
-}
+fetch("https://api.github.com/repos/jadinsa9-sudo/portfolio/commits")
+  .then(response => response.json())
+  .then(data => {
+    if (data.length > 0) {
+        displayDate(new Date(data[0].commit.committer.date));
+    }
+  });
